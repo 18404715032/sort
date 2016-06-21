3a9ef40ccc6d4d9c33c71d900085177a99d4e4a8
18404715032 <20141105066yh@admindeiMac-36.local>
2016-06-21 10:09:10 +0800

    排序
    在一个数组中定义20个数字，使用冒泡排序将数字从大到小排序，然后输出，在输出结果时将原来数组中的数字按原顺序输出一遍，再将新顺序输出一遍

--- sort/main.swift
+++ sort/main.swift
@@ -6,7 +6,32 @@
 //  Copyright © 2016年 杨浩. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+var array = [1,56,976,3,76,90,3,6,2,9,35,57,43,56,89,444,100,66,23,19]
 
-print("Hello, World!")
 
+
+print("排序前的值：")
+
+ for item in array
+ {
+     var ii = item
+     print(ii)
+     }
+
+ for i in 0 ..< array.count - 1 {
+    for j in 0 ..< array.count - 1 - i{
+         if array[j] > array[j + 1] {
+             var temp = array[j + 1]
+             array[j + 1] = array[j]
+             array[j] = temp
+             }
+         }
+     }
+ print("排序后的值：")
+
+ for item in array
+ {
+     var ii = item
+     print(ii)
+     }
